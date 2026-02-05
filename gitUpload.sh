e2ac8c709ee9ea697a6bfaabf5ba04137ada0751
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-02-05 20:34:55 -0300

    ci: proper pass an script to upload to git

---
+++ gitUpload.sh
@@ -0,0 +1,12 @@
+LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
+LAST_COMMIT_HASH=$(echo -n $LAST_COMMIT_MSG | sha256sum)
+COMMIT_MESSAGE="bot: $LAST_COMMIT_HASH"
+if [[ `git status --porcelain` ]]; then
+  git config --global user.name "$1"
+  git config --global user.email "$2"
+  git add . 
+  git commit -m $COMMIT_MESSAGE
+  git push
+else
+  echo "no changes to push"
+fi
