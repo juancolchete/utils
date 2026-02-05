c03843d84d49030c1541d2c7236319f4f8e55bee
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-02-05 20:44:33 -0300

    ci: safe dir as first thing
    Removed redundant commit message initialization and echo statement.

--- gitUpload.sh
+++ gitUpload.sh
@@ -1,9 +1,7 @@
+git config --global --add safe.directory $3
 LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
 LAST_COMMIT_HASH=$(echo -n $LAST_COMMIT_MSG | sha256sum)
 COMMIT_MESSAGE="bot: $LAST_COMMIT_HASH"
-COMMIT_MESSAGE="bot: "
-git config --global --add safe.directory $3
-echo "safe $3"
 if [[ `git status --porcelain` ]]; then
   git config --global user.name "$1"
   git config --global user.email "$2"
