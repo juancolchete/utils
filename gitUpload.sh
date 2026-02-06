59ea5736401083c626a6f774200c750db784659d
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-02-05 21:29:15 -0300

    ci: Add option to customize commit message in gitUpload.sh

--- gitUpload.sh
+++ gitUpload.sh
@@ -3,6 +3,9 @@ LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
 LAST_COMMIT_HASH=$(echo -n $LAST_COMMIT_MSG | sha256sum | base64)
 HASH_MAX_SIZE=20
 COMMIT_MESSAGE="bot: ${LAST_COMMIT_HASH:0:$HASH_MAX_SIZE}"
+if [[ ! -z "$4" ]]; then
+  COMMIT_MESSAGE=$4
+fi
 if [[ `git status --porcelain` ]]; then
   git config --global user.name "$1"
   git config --global user.email "$2"
